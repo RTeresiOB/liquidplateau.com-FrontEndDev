9f550b0c531174717dc83eded800754a7744920b
RTeresiOB <robert.teresi@yale.edu>
2021-10-03 18:00:26 -0400

    Committing before debug

--- src/About.js
+++ src/About.js
@@ -9,19 +9,25 @@ export default class About extends Component {
         return (
       <>
       <div className='about personal'>
-        <p> Hi. My name is Robert Teresi.
-          
-          Welcome to my personal website, where I will keep an updated accounting of what I've done,
-           what I'm working on, and where I'm headed next.
+        <p> Hi there!
+        </p>
+          <p> Welcome to my personal website, where I will keep an updated accounting of what I've done,
+           what I'm working on, and where I'm headed next. The website is still under construction,
+            so make sure to check back in for new content, projects, and features.
         </p>
         <p>
-        I am a research associate at the Yale School of Management.
+        My name is Robert Teresi and 
+        I am a data-intensive research associate at the Yale School of Management, working in the Organizational Behavior group. 
+          In my role, I have had the privilege of utilizing advanced algorithms and state-of-the-art data to 
+          answer new questions about human relationships and organizations on micro- and macro scales.
         </p>
-        
+        <p>
+        I'm fascinated by the newly emerging research and technologies in empirical methods and machine learning. In my work and in my data projects,
+         I aim to seek out original ways to employ these trends to discover new objects of inquiry, along with the corresponding instruments with which we can measure them.
+        </p>
       </div> 
       <div className='about drawing'>
-        Test
-      <SvgLines animate={ true } duration={ 500 }>
+      <SvgLines animate={ true } stagger={100} duration={ 30000 }>
       <svg 
           id="svgId" 
           xmlns="http://www.w3.org/2000/svg"
@@ -32,7 +38,39 @@ export default class About extends Component {
             height="100%"
             viewBox="0 0 800 600"
             preserveAspectRatio="none">
-      <path stroke="green" strokeWidth="10" fill="none" d="M20.8,51c0,0,20.8,18.2,21.5,18.2c0.6,0,33.3-38.5,33.3-38.5" />
+      <path stroke="black" strokeWidth="1" fill="none" d="M 300 0 L 0 100 " /> // X, Y, length
+      <path stroke="black" strokeWidth="1" fill="none" d="M 300 0 L 600 100 " />
+      <path stroke="black" strokeWidth="1" fill="none" d="M 300 600 L 0 100 " />
+      <path stroke="black" strokeWidth="1" fill="none" d="M 300 600 L 600 100 " />
+      <path stroke="black" strokeWidth="1" fill="none" d="M 300 0 L 0 200 " /> // X, Y, length
+      <path stroke="black" strokeWidth="1" fill="none" d="M 300 0 L 600 200 " />
+      <path stroke="black" strokeWidth="1" fill="none" d="M 300 600 L 0 200 " />
+      <path stroke="black" strokeWidth="1" fill="none" d="M 300 600 L 600 200 " />
+      <path stroke="black" strokeWidth="1" fill="none" d="M 300 0 L 0 300 " /> // X, Y, length
+      <path stroke="black" strokeWidth="1" fill="none" d="M 300 0 L 600 300 " />
+      <path stroke="black" strokeWidth="1" fill="none" d="M 300 600 L 0 300 " />
+      <path stroke="black" strokeWidth="1" fill="none" d="M 300 600 L 600 300 " />
+      <path stroke="black" strokeWidth="1" fill="none" d="M 300 0 L 0 400 " /> // X, Y, length
+      <path stroke="black" strokeWidth="1" fill="none" d="M 300 0 L 600 400 " />
+      <path stroke="black" strokeWidth="1" fill="none" d="M 300 600 L 0 400 " />
+      <path stroke="black" strokeWidth="1" fill="none" d="M 300 600 L 600 400 " />
+      <path stroke="black" strokeWidth="1" fill="none" d="M 300 0 L 0 500 " /> // X, Y, length
+      <path stroke="black" strokeWidth="1" fill="none" d="M 300 0 L 600 500 " />
+      <path stroke="black" strokeWidth="1" fill="none" d="M 300 600 L 0 500 " />
+      <path stroke="black" strokeWidth="1" fill="none" d="M 300 600 L 600 500 " />
+
+     // From center out 
+     <path stroke="black" strokeWidth="1" fill="none" d="M 300 300 L 0 100 " /> // X, Y, length
+      <path stroke="black" strokeWidth="1" fill="none" d="M 300 300 L 600 100 " />
+      <path stroke="black" strokeWidth="1" fill="none" d="M 300 300 L 0 200 " /> // X, Y, length
+      <path stroke="black" strokeWidth="1" fill="none" d="M 300 300 L 600 200 " />
+      <path stroke="black" strokeWidth="1" fill="none" d="M 300 300 L 0 300 " /> // X, Y, length
+      <path stroke="black" strokeWidth="1" fill="none" d="M 300 300 L 600 300 " />
+      <path stroke="black" strokeWidth="1" fill="none" d="M 300 300 L 0 400 " /> // X, Y, length
+      <path stroke="black" strokeWidth="1" fill="none" d="M 300 300 L 600 400 " />
+      <path stroke="black" strokeWidth="1" fill="none" d="M 300 300 L 0 500 " /> // X, Y, length
+      <path stroke="black" strokeWidth="1" fill="none" d="M 300 300 L 600 500 " />
+
         </svg>
         </SvgLines>
       </div>
