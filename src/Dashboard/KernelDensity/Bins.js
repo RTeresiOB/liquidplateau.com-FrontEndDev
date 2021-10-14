607ddd5df7ef0da37f092189c9cb45b08c09486c
RTeresiOB <robert.teresi@yale.edu>
2021-10-14 14:49:11 -0400

    Changed bad position directive (still not perfect)

--- src/Dashboard/KernelDensity/Bins.js
+++ src/Dashboard/KernelDensity/Bins.js
@@ -1,6 +1,5 @@
 /** Bins.js */
 import React from "react";
-import PropTypes from "prop-types";
 import * as d3 from "d3";
 
 const Bins = ({
@@ -9,10 +8,11 @@ const Bins = ({
     
     const handleOpacity = () =>
     {}
+    
     // Need to make a child for each bin
     const Bins = binData.map((bin, d) =>
-    <rect x={xScale(bin.x0) + 1} y={yScale(bin.length / data.length)} 
-    width={xScale(bin.x1) - xScale(bin.x0) - 1}
+    <rect x={xScale(bin.x0)} y={yScale(bin.length / data.length)} 
+    width={xScale(bin.x1) - xScale(bin.x0)}
     height={yScale(0) - yScale(bin.length / data.length)} 
     fill={d3.interpolateRdBu((xScale.domain()[1] - ((bin.x1 + bin.x0) / 2))/ (xScale.domain()[1] - xScale.domain()[0]))}
     opacity={.7}
