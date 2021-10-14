6fca516970522c893da3d36edb0012204a17cb49
RTeresiOB <robert.teresi@yale.edu>
2021-10-14 11:02:38 -0400

    Setting up context
    Can change context state with useReduce from table
    
    Next have it change from rule.
    
    Then have them sync.

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -2,10 +2,13 @@
 
 /* Controls orientation and data loading for all dashboard graphs */
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 import readPolData from "./KernelDensity/readPolData";
 import KernelDensityChart from "./KernelDensity/KernelDensityChart";
 import PolyTable from "./PolyTable/PolyTable"
+
+
+export const  SeletedPoliticianDispatch = React.createContext(null);
 export default function Dashboard(props) {
     // Define dimensions for kernelDensity (this will have to be scaled by window dimensions)
     const kernelDimensions = {
@@ -19,8 +22,14 @@ export default function Dashboard(props) {
         }
       };
 
+    const selectPoliticanReducer = (idx) =>
+      {
+          return {selectedPolitician:idx};
+      }
+
     const [isLoaded, setIsLoaded] = useState(false);
     const [data, setData] = useState(null);
+    const [selectedPolitician, setSelectedPolitician] = useReducer(selectPoliticanReducer,{selectedPolitician:null});
     if(data ===null){
         readPolData("data/scores.csv", false).then((result) => {return(result);}).then((result) =>
          {setData(result);});
@@ -35,10 +44,15 @@ export default function Dashboard(props) {
     },[data]);
 
     if(isLoaded){
+        console.log(setSelectedPolitician)
         return(
             <>
-                <KernelDensityChart data={data} dimensions={kernelDimensions} />
-                <PolyTable data={data} />
+                <SeletedPoliticianDispatch.Provider value={setSelectedPolitician}>
+                    <KernelDensityChart data={data} dimensions={kernelDimensions} 
+                                        politician={selectedPolitician} />
+                    <PolyTable  data={data}
+                                politician={selectedPolitician} />
+                </SeletedPoliticianDispatch.Provider>
             </>);
     } else{
         return(
--- src/Dashboard/PolyTable/ReactRow.js
+++ src/Dashboard/PolyTable/ReactRow.js
@@ -1,16 +1,21 @@
-import React, {useState} from "react"
+import React, {useState, useContext} from "react"
 import * as d3 from "d3";
+import {SeletedPoliticianDispatch} from "../Dashboard"
 export default function ReactRow(props){
 
+    const SeletedPoliticianDispatchContext = useContext(SeletedPoliticianDispatch);
+    console.log("Context:", SeletedPoliticianDispatchContext);
     const [color, setColor] = React.useState("");
     const [textColor, setTextColor] = React.useState("");
-    const onHover = (props) => {
+    const onHover = () => {
         props.cells.map(cell =>{
             if(Number(cell.value) == cell.value){
                 // For now so I don't have to bring in domain assume -2 to 2
                 var colVal = 1 - ((2 + parseFloat(cell.value))/4)
                 setColor(d3.interpolateRdBu(colVal));
                 setTextColor("white");
+                SeletedPoliticianDispatchContext(parseInt(cell.getCellProps().key.match(/\d+/)[0]));
+                //SelectedPoliticianDispatchContext (parseInt(cell.getCellProps().key.match(/\d+/)[0]));
             }
         });
     }
@@ -28,7 +33,7 @@ export default function ReactRow(props){
         <tr key={props.key}
             role={props.role}
             style={rowStyles}
-            onMouseEnter={() => onHover(props)}
+            onMouseEnter={() => onHover()}
             onMouseLeave={() => onLeave()}   >
                 {props.children}
         </tr> 
