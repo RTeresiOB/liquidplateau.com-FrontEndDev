54443bc7686058940cc2da08093c205cbaf5b2c0
RTeresiOB <robert.teresi@yale.edu>
2022-03-16 14:43:49 -0400

    Added some copy

--- src/About.js
+++ src/About.js
@@ -1,5 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component} from 'react';
 import SvgLines from 'react-mt-svg-lines';
+
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
+
 export default class About extends Component {
     constructor(props) {
         super(props);
@@ -9,8 +13,15 @@ export default class About extends Component {
         return (
       <>
       <div className='about personal'>
-        <p> Hi there!
-        </p>
+      <CarouselProvider
+        naturalSlideWidth={150}
+        naturalSlideHeight={100}
+        variableHeight={true}
+        totalSlides={3}
+        infinite={true}
+      >
+        <Slider>
+          <Slide index={0}  ><div >
           <p> Welcome to my personal website, where I will keep an updated accounting of what I've done,
            what I'm working on, and where I'm headed next. The website is still under construction,
             so make sure to check back in for new content, projects, and features.
@@ -19,15 +30,110 @@ export default class About extends Component {
         My name is Robert Teresi and 
         I am a data-intensive research associate at the Yale School of Management, working in the Organizational Behavior group. 
           In my role, I have had the privilege of utilizing advanced algorithms and state-of-the-art data to 
-          answer new questions about human relationships and organizations on micro- and macro scales.
+          answer new questions about human relationships and organizations on micro- and macro scales. 
         </p>
         <p>
-        I'm fascinated by the newly emerging research and technologies in empirical methods and machine learning. In my work and in my data projects,
-         I aim to seek out original ways to employ these trends to discover new objects of inquiry, along with the corresponding instruments with which we can measure them.
+        I'm driven by using data to learn more about the world and create products that make it a better place. 
         </p>
       </div> 
+      <div style={{display: 'flex',flexDirection: 'row', width: '95%',
+                   position: 'relative', justifyContent:'space-between'}}>
+        <ButtonBack>About This Website</ButtonBack>
+        <ButtonNext style={{ justifyContent: 'center', alignItems:'center'}}>Research Projects</ButtonNext> 
+      </div>
+      </Slide>
+          <Slide index={1} style={{overflowY: 'scroll'}} >
+            <h3>Research Projects</h3>
+            <br></br>
+            <p>Work team identification associated with less stress and burnout among front-line emergency department staff amid the COVID-19 pandemic (Sangal, Wrzesniewski, DiBenigno, Reid, Ulrich, Liebhardt, Bray, Yang, Eun, Venkatesh, King)
+            <br></br>
+          <em style={{"color":"#b8b8b8"}}> <a href="https://bmjleader.bmj.com/content/5/1/51">BMJ Leader </a> (2021) </em></p>
+          <ul style={{marginLeft:30,
+                        color:"black",
+                        fontWeight:100}}>
+              <li>Built automatic survey-sending service for Yale New Haven Hospital System that updated in real time with changes in the hospital's scheduling platform. </li>
+              <li>Sent surveys 15 minutes before the end of staff and faculty shifts during the peak of the first wave of the COVID pandemic to help understand and prevent sources of stress and burnout among physicians, residents, APPs, and nurses. </li>
+          </ul>
+          <br></br>
+          <p> Detrimental Effects of Organizations’ Instrumental Diversity Rhetoric for Underrepresented Group Members’ Sense of Belonging (<a href="https://som.yale.edu/faculty/oriane-m-georgeac">Georgeac</a>)
+ <br></br>
+          <em style={{"color":"#b8b8b8"}}> Journal of Personality and Social Psychology: Under Second Review </em></p>
+          <ul style={{marginLeft:30,
+                        color:"black",
+                        fontWeight:100}}>
+            <li>Improved performance of an existing logistic LASSO classifier algorithm, training the algorithm to learn the characteristics of 2 distinct types of rhetoric: the “fairness case” for diversity (e.g., “diversity at work is the right thing to do”) and the “business case” for diversity (e.g., “diversity at work is good for business”).</li>
+            <li> Used the trained classifier to run out-of-sample predictions on whether Fortune 500 companies’ online diversity statements corresponded to a “fairness” case or a “business case”, with the goal of investigating the current prevalence of these two diversity narratives in organizations.</li>
+          </ul>
+          <p>"Who's On Next?" How Attending Physician Speed is Slowed by the Presence of Faster Concurrently Staffed Physicians ( <a href="https://medicine.yale.edu/profile/rohit_sangal/?tab=research">Sangal</a>, Teresi, <a href="https://medicine.yale.edu/profile/arjun_venkatesh/"> Venkatesh</a>,<a href="https://som.yale.edu/faculty/marissa-king">King</a>)<br></br> <em style={{"color":"#b8b8b8"}}> Under Review at Annals of Emergency Medicine</em></p>
+          <ul style={{marginLeft:30,
+                        color:"black",
+                        fontWeight:100}}>
+              <li>Created predicted metric of patient care time and relative physician treatment speed from Yale New Haven Hospital Emergency Department internal dashboards across multiple campuses.</li>
+              <li>Utilizing random assignment of patients to doctor speed, we estimate a decrease in primary attending treatment speed, increase in patients treated, and decrease in scans ordered when concurrently staffed physicians are faster.</li>
+          </ul>
+          <br></br>
+          <p> Cognitive Frames on Racism and Political Ideology: Evidence from Twitter Activity around the Black Lives Matter Movement ( <a href="https://cydneydupree.com/">Dupree</a>, Teresi, <a href="https://www.linkedin.com/in/leilah-harouni"> Harouni</a>)<br></br> <em style={{"color":"#b8b8b8"}}> Draft in Progress </em></p>
+          <ul style={{marginLeft:30,
+                        color:"black",
+                        fontWeight:100}}>
+              <li>On all tweets that joined the #blacklivesmatter conversatioin (as well as those with similar hashtags), explored how the intersection of race, age, gender, and political ideology influence how people frame discussions on race on an axis of personal (e.g. biases, microgression, and community) vs. structural (elections, police reform, and structural racism).</li>
+              <li>Coordinated twitter API calls across 30+ twitter API accounts within the department across as many remote virtual machines.</li>
+              </ul>
+          <br></br>
+          <p>Entrepreneurship Among Real Estate Agents (<a href="https://som.yale.edu/faculty/tristan-l-botelho">Botelho</a>, <a href="https://www.linkedin.com/in/geoffreyborchhardt">Borchhardt</a>, Teresi) <em style={{"color":"#b8b8b8"}}> <br></br>Data analysis in progress</em></p>
+            <ul style={{marginLeft:30,
+                        color:"black",
+                        fontWeight:100}}>
+              <li>Using real estate listing data from Multiple Listing Services databases across America from 2000-2015 to uncover patterns entreprerneurship among real estate agents. </li>
+              <li>Answering questions of who starts their own firms and measuring the returns on entrepreneurial endeavours.</li>
+            
+            </ul>
+            <br></br>
+            <p>Doctor-Prescriber Networks and Opioid Overprescription Defusion (<a href="https://som.yale.edu/faculty/marissa-king">King</a>, Teresi) <em style={{"color":"#b8b8b8"}}> <br></br>Data analysis in progress</em></p>
+            <ul style={{marginLeft:30,
+                        color:"black",
+                        fontWeight:100}}>
+              <li> Using a comprehensive dataset of pharmacy data across the United States, created doctor-prescriber networks within hostpital referral regions (a shared tie corresponds to two doctors who gave a prescription to at least one common patient). </li>
+              <li> Exploiting geographic movements across hospital referral regions, exploring whether doctors' network prominence (eigencentrality, betweenness centrality, degree, etc.) influence whether doctors in their destination hospital referral region adopt their opioid prescription behavior. </li>
+            
+            </ul>
+            <br></br>
+            <p>Effectiveness of Mobile Text-Message Reminders in Increasing Resident Feedback (Sun, Coughlin, Della-Giustina, Goldflam, Sangal, Teresi, Bathgate, Tsyrulnik) <em style={{"color":"#b8b8b8"}}> <br></br>Data Collection in Progress</em></p>
+            <ul style={{marginLeft:30,
+                        color:"black",
+                        fontWeight:100}}>
+              <li> A study within Yale New Haven Hospitals Emergency Departments aiming to improve physician feedback on resident performance.</li>
+              <li> Utilized code I wrote to send out text messages at the end of staff and faculty shifts to send out reminders to fill out feedback forms.</li>
+              <li> Results from first stage of the study (text messages to residents only) doubled completion of feedback forms. </li>
+            
+            </ul>
+            <br></br>
+          <div style={{display: 'flex',flexDirection: 'row', width: '95%',
+                   position: 'relative', justifyContent:'space-between'}}>
+          <ButtonBack><div>About Me</div></ButtonBack>
+          <ButtonNext style={{ justifyContent: 'center', alignItems:'center'}}>About This Website</ButtonNext> 
+          </div>
+          </Slide>
+          <Slide index={2}>
+            <p>This website is a single-page React app, written by me.</p>
+            <p>On my contact page, you will find links to my socials and my email address, as well as a form with which you can send me a message directly on this website implemented using PHP.</p>
+            <p>On the twitter ideology page, you can find a dashboard I created for one of my active research projects (more info in my projects page and the dashboard page itself).
+               The dashboard was written with d3, optimizing the library for use with React by creating cross-graph reactivity and reusable, dynamic graph components.
+               The backend of the dashboard delivers results from a sci-kit learn machine learning pipeline via a Python Flask app.</p>
+            
+            <div style={{display: 'flex',flexDirection: 'row', width: '95%',
+                position: 'relative', justifyContent:'space-between'}}>
+          <ButtonBack><div>Research Projects</div></ButtonBack>
+          <ButtonNext style={{ justifyContent: 'center', alignItems:'center'}}>About Me</ButtonNext> 
+          </div>
+            </Slide>
+        </Slider>
+        
+        
+      </CarouselProvider>
+      </div>
       <div className='about drawing'>
-      <SvgLines animate={ true } stagger={100} duration={ 30000 }>
+      <SvgLines animate={ true } stagger={100} duration={ 30000 } >
       <svg 
           id="svgId" 
           xmlns="http://www.w3.org/2000/svg"
@@ -36,7 +142,7 @@ export default class About extends Component {
             y="0"
             width="100%"
             height="100%"
-            viewBox="0 0 800 600"
+            viewBox="-100 0 800 600"
             preserveAspectRatio="none">
       <path stroke="black" strokeWidth="1" fill="none" d="M 300 0 L 0 100 " /> // X, Y, length
       <path stroke="black" strokeWidth="1" fill="none" d="M 300 0 L 600 100 " />
