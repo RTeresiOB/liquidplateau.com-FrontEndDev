3937f49387c4be83ba8cb6effd2051e24eb3ec96
RTeresiOB <robert.teresi@yale.edu>
2022-03-23 12:56:49 -0400

    Update Resume

--- src/Resume.js
+++ src/Resume.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import Resume_Teresi from './assets/Resume_Teresi.png'
+import Resume_Teresi from './assets/Teresi_Resume_Mar_2022.png'
 import DownloadLink from "react-download-link";
 const Resume = (props) => {
         return (
-      <div justifyContent='center'>   
-        <img src={Resume_Teresi} style={{
+      <div style={{justifyContent:'center', display:'block'}}> 
+      <p style={{fontFamily:'Avenir', textAlign:'center', marginBottom:0, marginTop:15, padding:0}}> Download .pdf <a  href="https://drive.google.com/file/d/157ggMhfWXV1VynfFstE69_7Varh1fAia/view">here.</a></p>
+        
+      <img src={Resume_Teresi} style={{
     display:'block',
     margin: '0 auto',
     flex: 1,
@@ -13,6 +15,7 @@ const Resume = (props) => {
     resizeMode: 'contain',
     display:'contain',
 }} alt="Resume" />
+  
       </div>)
 }
 
